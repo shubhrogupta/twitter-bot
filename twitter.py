d2a241ed9917730bad84ae47dfbb62ba9dd3a1b9
shubhro <shubhrogupta117@gmail.com>
2020-01-26 21:47:15 +0530

    heh

---
+++ twitter.py
@@ -0,0 +1,7 @@
+import tweepy
+import time
+
+auth = tweepy.OAuthHandler('B2hri15fL3UKlq5Kty8fObqy0','Xztav1G3nXqhXSHuuQ3UEXRi2BIgglskDRuL2kJFaF97KHSOMI')
+
+auth.set_access_token('819554966998282240-WXfd4GqpDh1uUBlV13SK3xt6ZY2mnaY', 'MWBrwVFfISXKcHwoAwtLpiCCVbhqVWPztm7Za5Wt65Nwh')
+
