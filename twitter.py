4c910185ed23a8b2ea0805de52a768b42c9689b8
shubhro <shubhrogupta117@gmail.com>
2020-01-26 22:59:36 +0530

    hehe

--- twitter.py
+++ twitter.py
@@ -5,3 +5,8 @@ auth = tweepy.OAuthHandler('B2hri15fL3UKlq5Kty8fObqy0','Xztav1G3nXqhXSHuuQ3UEXRi
 
 auth.set_access_token('819554966998282240-WXfd4GqpDh1uUBlV13SK3xt6ZY2mnaY', 'MWBrwVFfISXKcHwoAwtLpiCCVbhqVWPztm7Za5Wt65Nwh')
 
+api = tweepy.API(auth, wait_on_rate_limit= True, wait_on_rate_limit_notify= True)
+
+user = api.me()
+
+for follower in tweepy.Cursor
